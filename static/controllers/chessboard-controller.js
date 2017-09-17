0284f6c802286d387cfe1b93fbd479497979ffa6
Aaron Klein <ajmath62@gmail.com>
2017-09-17 16:07:34 -0400

    Created an Angular app to direct the webpage

---
+++ static/controllers/chessboard-controller.js
@@ -0,0 +1,32 @@
+(function(){
+    angular
+    .module("app", [])
+    .controller("ChessboardController", function(){
+        var chessboard = this;
+
+        chessboard.rows = [8, 7, 6, 5, 4, 3, 2, 1];
+        chessboard.columns = ["A", "B", "C", "D", "E", "F", "G", "H"];
+        chessboard.pieces = {"A1": "white rook", "B1": "white knight", "C1": "white bishop",
+        "D1": "white queen", "E1": "white king", "F1": "white bishop", "G1": "white knight",
+        "H1": "white rook", "A2": "white pawn", "B2": "white pawn", "C2": "white pawn",
+        "D2": "white pawn", "E2": "white pawn", "F2": "white pawn", "G2": "white pawn",
+        "H2": "white pawn", "A7": "black pawn", "B7": "black pawn", "C7": "black pawn",
+        "D7": "black pawn", "E7": "black pawn", "F7": "black pawn", "G7": "black pawn",
+        "H7": "black pawn", "A8": "black rook", "B8": "black knight", "C8": "black bishop",
+        "D8": "black queen", "E8": "black king", "F8": "black bishop", "G8": "black knight",
+        "H8": "black rook"};  // Starting position
+        chessboard.pieceStyle = "letter";
+
+        chessboard.switchPieceStyles = function() {
+            if (chessboard.pieceStyle == "letter") {
+                chessboard.pieceStyle = "symbol";
+                $("#switch-piece-styles").text("Switch to piece letter abbreviations");
+            }
+            else if (chessboard.pieceStyle == "symbol") {
+                chessboard.pieceStyle = "letter";
+                $("#switch-piece-styles").text("Switch to piece symbols");
+            }
+        };
+
+    });
+}());
