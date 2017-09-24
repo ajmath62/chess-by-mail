a17c94a24d09cd3b4e694947ee526f024b7d326d
Aaron Klein <ajmath62@gmail.com>
2017-09-24 15:54:01 -0400

    Changed variable scopes, reduced DOM interaction with controller

--- public/static/chess-utils.js
+++ public/static/chess-utils.js
@@ -1,4 +1,5 @@
 pieceNameList = ["pawn", "rook", "knight", "bishop", "queen", "king"];
+$scope = {};
 
 function contains(array, object){
     // Return true if array exists and is indexable and contains object
@@ -7,8 +8,8 @@ function contains(array, object){
 
 function getNeighboringSquare(startSquare, directions) {
     [oldColumn, oldRow] = startSquare;
-    newColumn = String.fromCharCode(oldColumn.charCodeAt() + directions[0]);
-    newRow = parseInt(oldRow) + directions[1];
+    var newColumn = String.fromCharCode(oldColumn.charCodeAt() + directions[0]);
+    var newRow = parseInt(oldRow) + directions[1];
     if (!contains("ABCDEFGH", newColumn)) {
         // If the column is not a properly named column
         return undefined;
@@ -25,14 +26,14 @@ function getNeighboringSquare(startSquare, directions) {
 function getSquareDistance(startSquare, endSquare) {
     [startColumn, startRow] = startSquare;
     [endColumn, endRow] = endSquare;
-    numColumns = endColumn.charCodeAt() - startColumn.charCodeAt();
-    numRows = endRow - startRow;  // this is an Int
+    var numColumns = endColumn.charCodeAt() - startColumn.charCodeAt();
+    var numRows = endRow - startRow;  // this is an Int
     return [numColumns, numRows];
 }
 
 function findPiece(boardState, pieceType) {
     // This returns a list, regardless of whether the piece is unique or not
-    squareList = [];
+    var squareList = [];
     for (let [square, piece] of Object.entries(boardState)) {
         if (piece === pieceType)
             squareList.push(square);
@@ -48,9 +49,9 @@ function getOtherColor(color) {
 }
 
 function isPieceMovable(boardState, startSquare, endSquare, castleLegality) {
-    pieceType = boardState[startSquare];
-    pieceColor = pieceType.split(" ")[0];
-    moveDistance = getSquareDistance(startSquare, endSquare);
+    var pieceType = boardState[startSquare];
+    var pieceColor = pieceType.split(" ")[0];
+    var moveDistance = getSquareDistance(startSquare, endSquare);
 
     switch (pieceType) {
     case "white pawn":
@@ -81,22 +82,22 @@ function isPieceMovable(boardState, startSquare, endSquare, castleLegality) {
     case "white rook":
     case "black rook":
         if (moveDistance[0] === 0) {
-            moveDirection = [0, Math.sign(moveDistance[1])]
-            moveLength = Math.abs(moveDistance[1])
+            var moveDirection = [0, Math.sign(moveDistance[1])]
+            var moveLength = Math.abs(moveDistance[1])
             if (moveDistance[1] === 0)
                 // The rook can't move to its own space
                 return false;
         }
         else if (moveDistance[1] === 0) {
-            moveDirection = [Math.sign(moveDistance[0]), 0]
-            moveLength = Math.abs(moveDistance[0])
+            var moveDirection = [Math.sign(moveDistance[0]), 0]
+            var moveLength = Math.abs(moveDistance[0])
         }
         else
             // The rook can only move along a rank or a file
             return false;
 
         for (i = 1; i < moveLength; i ++) {
-            checkDistance = [moveDirection[0]*i, moveDirection[1]*i]
+            var checkDistance = [moveDirection[0]*i, moveDirection[1]*i]
             if (boardState[getNeighboringSquare(startSquare, checkDistance)])
                 // The rook cannot move through other pieces
                 return false;
@@ -120,14 +121,14 @@ function isPieceMovable(boardState, startSquare, endSquare, castleLegality) {
         }
     case "white bishop":
     case "black bishop":
-        moveLength = Math.abs(moveDistance[0])
+        var moveLength = Math.abs(moveDistance[0])
         if (moveLength === 0 || moveLength !== Math.abs(moveDistance[1]))
             // The bishop can only move along a perfect diagonal
             return false;
 
-        moveDirection = [Math.sign(moveDistance[0]), Math.sign(moveDistance[1])]
+        var moveDirection = [Math.sign(moveDistance[0]), Math.sign(moveDistance[1])]
         for (i = 1; i < moveLength; i ++) {
-            checkDistance = [moveDirection[0]*i, moveDirection[1]*i]
+            var checkDistance = [moveDirection[0]*i, moveDirection[1]*i]
             if (boardState[getNeighboringSquare(startSquare, checkDistance)])
                 // The bishop cannot move through other pieces
                 return false;
@@ -137,26 +138,26 @@ function isPieceMovable(boardState, startSquare, endSquare, castleLegality) {
     case "white queen":
     case "black queen":
         if (moveDistance[0] === 0) {
-            moveDirection = [0, Math.sign(moveDistance[1])]
-            moveLength = Math.abs(moveDistance[1])
+            var moveDirection = [0, Math.sign(moveDistance[1])]
+            var moveLength = Math.abs(moveDistance[1])
             if (moveDistance[1] === 0)
                 // The queen can't move to its own space
                 return false;
         }
         else if (moveDistance[1] === 0) {
-            moveDirection = [Math.sign(moveDistance[0]), 0]
-            moveLength = Math.abs(moveDistance[0])
+            var moveDirection = [Math.sign(moveDistance[0]), 0]
+            var moveLength = Math.abs(moveDistance[0])
         }
         else {
-            moveLength = Math.abs(moveDistance[0])
+            var moveLength = Math.abs(moveDistance[0])
             if (moveLength === 0 || moveLength !== Math.abs(moveDistance[1]))
                 // The queen can only move along a rank, file or perfect diagonal
                 return false;
         }
 
-        moveDirection = [Math.sign(moveDistance[0]), Math.sign(moveDistance[1])]
+        var moveDirection = [Math.sign(moveDistance[0]), Math.sign(moveDistance[1])]
         for (i = 1; i < moveLength; i ++) {
-            checkDistance = [moveDirection[0]*i, moveDirection[1]*i]
+            var checkDistance = [moveDirection[0]*i, moveDirection[1]*i]
             if (boardState[getNeighboringSquare(startSquare, checkDistance)])
                 // The queen cannot move through other pieces
                 return false;
@@ -187,12 +188,12 @@ function isPieceMovable(boardState, startSquare, endSquare, castleLegality) {
                     || boardState[getNeighboringSquare(startSquare, [2, 0])])
                 return false;
             else {
-                threatColor = getOtherColor(pieceType.split(" ")[0]);
+                var threatColor = getOtherColor(pieceType.split(" ")[0]);
                 // Check that there are no threats to the king, the rook, or the square the rook is moving to
                 // Don't look at the square the king is moving to, that gets looked at later
                 for (let moveDistance of [[0, 0], [1, 0], [3, 0]]) {
-                    squareToCheck = getNeighboringSquare(startSquare, moveDistance)
-                    threatSquare = checkThreat(boardState, squareToCheck, threatColor);
+                    var squareToCheck = getNeighboringSquare(startSquare, moveDistance)
+                    var threatSquare = checkThreat(boardState, squareToCheck, threatColor);
                     if (threatSquare)
                         return [false, ["check", [threatSquare, squareToCheck]]];
                 }
@@ -207,12 +208,12 @@ function isPieceMovable(boardState, startSquare, endSquare, castleLegality) {
                     || boardState[getNeighboringSquare(startSquare, [-3, 0])])
                 return false;
             else {
-                threatColor = getOtherColor(pieceType.split(" ")[0]);
+                var threatColor = getOtherColor(pieceType.split(" ")[0]);
                 // Check that there are no threats to the king, the rook, or the square the rook is moving to
                 // Don't look at the square the king is moving to, that gets looked at later
                 for (let moveDistance of [[0, 0], [-1, 0], [-4, 0]]) {
-                    squareToCheck = getNeighboringSquare(startSquare, moveDistance)
-                    threatSquare = checkThreat(boardState, squareToCheck, threatColor);
+                    var squareToCheck = getNeighboringSquare(startSquare, moveDistance)
+                    var threatSquare = checkThreat(boardState, squareToCheck, threatColor);
                     if (threatSquare)
                         return [false, ["check", [threatSquare, squareToCheck]]];
                 }
@@ -226,14 +227,21 @@ function isPieceMovable(boardState, startSquare, endSquare, castleLegality) {
     }
 }
 
+function makeTestMove(boardState, startSquare, endSquare) {
+    // Move the piece, capturing if necessary
+    var pieceType = boardState[startSquare];
+    delete boardState[startSquare];
+    boardState[endSquare] = pieceType;
+}
+
 function checkThreat(boardState, square, color) {
     // Check if there is a threat to the given square from the given color
 
     // A king can't threaten from two spaces away
-    castleLegality = {"A": false, "H": false}
+    var castleLegality = {"A": false, "H": false}
 
     for (let pieceName of pieceNameList) {
-        pieceType = color + " " + pieceName;
+        var pieceType = color + " " + pieceName;
         for (let pieceLoc of findPiece(boardState, pieceType)) {
             // If any piece is threatening the square, return its location
             if (isPieceMovable(boardState, pieceLoc, square, castleLegality))
@@ -247,23 +255,21 @@ function checkThreat(boardState, square, color) {
 
 function checkCheck(boardState, color) {
     [kingLoc] = findPiece(boardState, color + " king");
-    otherColor = getOtherColor(color)
+    var otherColor = getOtherColor(color)
     return checkThreat(boardState, kingLoc, otherColor);
 }
 
-function checkMoveValidity(boardInfo, endSquare) {
-    // AJK TODO make this stuff more global
-    boardState = boardInfo.pieces;
-    startSquare = boardInfo.firstClick;
-    currentPlayer = boardInfo.currentPlayer;
-    castleLegality = boardInfo.castleLegality[currentPlayer];
+function checkMoveValidity(startSquare, endSquare) {
+    $scope = window.angular.element($("#chessboard")).scope()
+    var currentPlayer = $scope.currentPlayer;
+    var castleLegality = $scope.castleLegality[currentPlayer];
 
-    if (boardState[startSquare].split(" ")[0] !== currentPlayer)
+    if ($scope.pieces[startSquare].split(" ")[0] !== currentPlayer)
         // A player can only move their own pieces
         return [false, ["turn", null]];
 
     // Make sure a piece can actually make the move specified
-    moveLegality = isPieceMovable(boardState, startSquare, endSquare, castleLegality)
+    var moveLegality = isPieceMovable($scope.pieces, startSquare, endSquare, castleLegality)
     if (!moveLegality)
         return [false, ["illegal", null]];
     else if (moveLegality[0] === false)
@@ -271,12 +277,12 @@ function checkMoveValidity(boardInfo, endSquare) {
         return [false, moveLegality[1]];
 
     // Test out the move before actually making it to see if any issues arise
-    boardCopy = {};
-    for (let [square, pieceType] of Object.entries(boardState))
+    var boardCopy = {};
+    for (let [square, pieceType] of Object.entries($scope.pieces))
         boardCopy[square] = pieceType;
-    makeMove(boardCopy, startSquare, endSquare);
-    checkingSquare = checkCheck(boardCopy, currentPlayer);
-    kingSquare = findPiece(boardCopy, currentPlayer + " king")
+    makeTestMove(boardCopy, startSquare, endSquare);
+    var checkingSquare = checkCheck(boardCopy, currentPlayer);
+    var kingSquare = findPiece(boardCopy, currentPlayer + " king")
     if (checkingSquare)
         // Don't let a player make a move that will put them in check or leave them in check
         return [false, ["check", [checkingSquare, kingSquare]]];
@@ -285,33 +291,35 @@ function checkMoveValidity(boardInfo, endSquare) {
     // If any rooks were moved or captured, don't allow future castling on that side
     // AJK TODO make this a subroutine, please
     if (startSquare === "A1" || endSquare === "A1")
-        boardInfo.castleLegality.white.A = false;
+        $scope.castleLegality.white.A = false;
     if (startSquare === "H1" || endSquare === "H1")
-        boardInfo.castleLegality.white.H = false;
+        $scope.castleLegality.white.H = false;
     if (startSquare === "A8" || endSquare === "A8")
-        boardInfo.castleLegality.black.A = false;
+        $scope.castleLegality.black.A = false;
     if (startSquare === "H8" || endSquare === "H8")
-        boardInfo.castleLegality.black.H = false;
-    if (boardState[startSquare] === "white king") {
-        boardInfo.castleLegality.white.A = false;
-        boardInfo.castleLegality.white.H = false;
+        $scope.castleLegality.black.H = false;
+    if ($scope.pieces[startSquare] === "white king") {
+        $scope.castleLegality.white.A = false;
+        $scope.castleLegality.white.H = false;
     }
-    if (boardState[startSquare] === "black king") {
-        boardInfo.castleLegality.black.A = false;
-        boardInfo.castleLegality.black.H = false;
+    if ($scope.pieces[startSquare] === "black king") {
+        $scope.castleLegality.black.A = false;
+        $scope.castleLegality.black.H = false;
     }
     // Allow the move to be made and pass along any comments from isPieceMovable
     return [true, moveLegality[1]];
 }
 
-function makeMove(boardState, startSquare, endSquare) {
+function makeMove(startSquare, endSquare) {
     // Move the piece, capturing if necessary
-    pieceType = boardState[startSquare];
-    delete boardState[startSquare];
-    boardState[endSquare] = pieceType;
+    var pieceType = $scope.pieces[startSquare];
+    delete $scope.pieces[startSquare];
+    $scope.pieces[endSquare] = pieceType;
 }
 
-function checkPromotion(boardState, square) {
+function checkPromotion() {
+    var boardState = $scope.pieces;
+    var square = $scope.lastMove[1];
     // Return true if the piece is a pawn on its last rank and false otherwise
     [pieceColor, pieceName] = boardState[square].split(" ");
     if (((pieceColor === "white" && square[1] == 8) || (pieceColor === "black" && square[1] == 1)) && pieceName === "pawn")
--- public/static/controllers/chessboard-controller.js
+++ public/static/controllers/chessboard-controller.js
@@ -1,12 +1,10 @@
 (function(){
     angular
     .module("app", [])
-    .controller("ChessboardController", function(){
-        var chessboard = this;
-
-        chessboard.rows = [8, 7, 6, 5, 4, 3, 2, 1];
-        chessboard.columns = ["A", "B", "C", "D", "E", "F", "G", "H"];
-        chessboard.pieces = {"A1": "white rook", "B1": "white knight", "C1": "white bishop",
+    .controller("ChessboardController", ["$scope", function($scope){
+        $scope.rows = [8, 7, 6, 5, 4, 3, 2, 1];
+        $scope.columns = ["A", "B", "C", "D", "E", "F", "G", "H"];
+        $scope.pieces = {"A1": "white rook", "B1": "white knight", "C1": "white bishop",
         "D1": "white queen", "E1": "white king", "F1": "white bishop", "G1": "white knight",
         "H1": "white rook", "A2": "white pawn", "B2": "white pawn", "C2": "white pawn",
         "D2": "white pawn", "E2": "white pawn", "F2": "white pawn", "G2": "white pawn",
@@ -15,57 +13,42 @@
         "H7": "black pawn", "A8": "black rook", "B8": "black knight", "C8": "black bishop",
         "D8": "black queen", "E8": "black king", "F8": "black bishop", "G8": "black knight",
         "H8": "black rook"};  // Starting position
-        chessboard.pieceStyle = "symbol";
-        chessboard.lastMove = ["A1", "A1"];
-        chessboard.currentPlayer = "white";
-        chessboard.castleLegality = {"white": {"A": true, "H": true}, "black": {"A": true, "H": true}};
+        $scope.pieceStyle = "symbol";
+        $scope.promotablePawn = "";
+        $scope.lastMove = ["", ""];
+        $scope.currentPlayer = "white";
+        $scope.castleLegality = {"white": {"A": true, "H": true}, "black": {"A": true, "H": true}};
 
-        chessboard.switchPieceStyles = function() {
-            if (chessboard.pieceStyle == "letter") {
-                chessboard.pieceStyle = "symbol";
-                $("#switch-piece-styles").text("Switch to piece letter abbreviations");
-            }
-            else if (chessboard.pieceStyle == "symbol") {
-                chessboard.pieceStyle = "letter";
-                $("#switch-piece-styles").text("Switch to piece symbols");
-            }
-        };
-
-        chessboard.firstClick = null;
-        chessboard.clickSquare = function(squareName) {
-            if (chessboard.firstClick === null) {
+        // AJK TODO look into directives for these and/or the chess-utils functions
+        $scope.firstClick = null;
+        $scope.clickSquare = function(squareName) {
+            if ($scope.firstClick === null) {
                 // If there is a piece on the chosen square, mark the square as selected
-                if (chessboard.pieces[squareName]) {
-                    chessboard.firstClick = squareName;
-                    $("#" + squareName).addClass("selected");
+                if ($scope.pieces[squareName]) {
+                    $scope.firstClick = squareName;
                 }
             }
             else {
                 // Attempt to move the previously selected piece to the newly chosen square
-                pieceType = chessboard.pieces[chessboard.firstClick];
-                [moveValidity, comments] = checkMoveValidity(chessboard, squareName);
+                var pieceType = $scope.pieces[$scope.firstClick];
+                [moveValidity, comments] = checkMoveValidity($scope.firstClick, squareName);
                 if (moveValidity) {
-                    makeMove(chessboard.pieces, chessboard.firstClick, squareName);
-                    $("#" + chessboard.lastMove[0]).removeClass("last-move")
-                    $("#" + chessboard.lastMove[1]).removeClass("last-move")
-                    chessboard.lastMove = [chessboard.firstClick, squareName];
-                    $("#" + chessboard.lastMove[0]).addClass("last-move")
-                    $("#" + chessboard.lastMove[1]).addClass("last-move")
+                    makeMove($scope.firstClick, squareName);
+                    $scope.lastMove = [$scope.firstClick, squareName];
 
-                    if (checkPromotion(chessboard.pieces, squareName)) {
-                        chessboard.promotablePawn = squareName;
-                        $("#promotion-prompt").show();
+                    if (checkPromotion()) {
+                        $scope.promotablePawn = squareName;
                     }
                     else
                         // If there is a promotion, flip whose turn it is after selecting which
                         // piece to promote to. Otherwise, flip whose turn it is now.
-                        chessboard.currentPlayer = getOtherColor(chessboard.currentPlayer);
+                        $scope.currentPlayer = getOtherColor($scope.currentPlayer);
 
                     // If the move was a castle, move the rook
                     if (comments === "castle-king")
-                        makeMove(chessboard.pieces, getNeighboringSquare(squareName, [1, 0]), getNeighboringSquare(squareName, [-1, 0]));
+                        makeMove(getNeighboringSquare(squareName, [1, 0]), getNeighboringSquare(squareName, [-1, 0]));
                     else if (comments === "castle-queen")
-                        makeMove(chessboard.pieces, getNeighboringSquare(squareName, [-2, 0]), getNeighboringSquare(squareName, [1, 0]));
+                        makeMove(getNeighboringSquare(squareName, [-2, 0]), getNeighboringSquare(squareName, [1, 0]));
                 }
                 else {
                     [errorType, errorDetails] = comments;
@@ -78,16 +61,15 @@
                     }
                 }
                 // Whether the move is valid or not, deselect the piece.
-                $("#" + chessboard.firstClick).removeClass("selected");
-                chessboard.firstClick = null;
+                $scope.firstClick = null;
             }
         };
 
-        chessboard.promote = function(pieceName) {
+        $scope.promote = function(pieceName) {
             // AJK TODO prevent the player from doing anything else in the meanwhile
-            chessboard.pieces[chessboard.promotablePawn] = chessboard.currentPlayer + " " + pieceName;
-            chessboard.currentPlayer = getOtherColor(chessboard.currentPlayer);
-            $("#promotion-prompt").hide();
+            $scope.pieces[$scope.promotablePawn] = $scope.currentPlayer + " " + pieceName;
+            $scope.currentPlayer = getOtherColor($scope.currentPlayer);
+            $scope.promotablePawn = "";
         }
-    });
+    }]);
 }());
